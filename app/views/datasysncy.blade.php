7bded69a5b5d0663033aeee0df527e13b566c7ed
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2014-09-22 23:43:25 +0300

    adding data syncy

--- app/views/datasysncy.blade.php
+++ app/views/datasysncy.blade.php
@@ -35,11 +35,7 @@ Sending Data To Server
     </div>
     <div id="output"></div>
     <div id="waiting"></div>
-    <div class="progress">
-        <div class="progress-bar progress-bar-striped active"  role="progressbar" aria-valuenow="10" aria-valuemin="100" aria-valuemax="100" style="width: 100%">
-            <span class="sr-only">100% Complete</span>
-        </div>
-    </div>
+
 </div>
 <script>
     $(document).ready(function(){
